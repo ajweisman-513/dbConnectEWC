b467bab471f070b5ab53c629bc219c4fa0d874e0
Alex Weisman <alex@upfrontworks.com>
2021-05-27 12:52:14 -0700

    working on patch to MongoConnection

--- routes/smarterObjects.js
+++ routes/smarterObjects.js
@@ -54,8 +54,13 @@ router.post('/', async (req, res) => {
 router.patch('/update/:id', async (req, res) => {
     console.log("id for this is", req.params)
     console.log("req.body", req.body)
-    const id = req.params.id
-    const macroArray = req.body.macroObjectType
+    console.log("req.body.location_id", req.body.location_id)
+    console.log("req.body.macroObject_id", req.body.macroObject_id)
+    const sO_id = req.params.id
+    const location_id = req.body.location_id
+    const macroObject_id = req.body.macroObject_id
+    const macroObjectType = req.body.macroObjectType
+    const newIsCompleteStatus = req.body.newIsCompleteStatus
     const acctName = req.body.acctName
     const collectionName = acctName + '-SmarterObject'
     const DynamicSmarterObjCollectionName = mongoose.model(
@@ -64,15 +69,16 @@ router.patch('/update/:id', async (req, res) => {
         collectionName
     )    
     await DynamicSmarterObjCollectionName
-        .findById(id, function (err, foundObject) {
+        .findById(sO_id, function (err, foundObject) {
             if (err) return res.status(500).send(err)
             if (!foundObject) res.status(404).send("Not Found")
             if (foundObject) {
-                let location = foundObject.locationObjs.id(req.body.locationObj)
-                let macroObjToUpdate = location[macroArray].id(req.body.macroObject_Id)
-                if (req.body.newIsCompleteStatus === false) {
+                let location = foundObject.locationObjs.id(location_id)
+                console.log(location)
+                let macroObjToUpdate = location[macroObjectType].id(macroObject_id)
+                if (newIsCompleteStatus === false) {
                     macroObjToUpdate._isComplete = false
-                    macroObjToUpdate._isCompletedAs = ''
+                    macroObjToUpdate._isCompletedAs = null
                     return res.status(200).send(
                         {
                             status: "item reset to default status",
