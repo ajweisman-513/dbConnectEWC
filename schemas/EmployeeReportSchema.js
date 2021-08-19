ef52eadcceab974b692e8944ad0c28da619f5606
Alex Weisman <alex@upfrontworks.com>
2021-08-19 11:52:51 -0700

    made mose fields on EmployeeReportSchema required:FALSE for testing

--- schemas/EmployeeReportSchema.js
+++ schemas/EmployeeReportSchema.js
@@ -4,9 +4,9 @@ const { Schema } = mongoose;
 const EmployeeReportSchema = new Schema({
     _reportType: { type: String, required: true },
     _acctName: { type: String, required: true },
-    _originatingReportName: { type: String, required: true },
+    _originatingReportName: { type: String, required: false },
     _mongoDocumentName: { type: String, required: false },
-    _reportDate: { type: String, required: true },
+    _reportDate: { type: String, required: false },
     _pRepresentation: { type: String, required: false },
     _data: { type: Array, required: true }
 }, {
